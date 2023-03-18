54ad512954ac9df513d6193f3942e2d5c7906256
Xiaodong_Wang <dongzhixiaodong@gmail.com>
2023-03-18 14:28:02 +0800

    注释utils.py

--- tensorflow_code/utils.py
+++ tensorflow_code/utils.py
@@ -50,33 +50,50 @@ def split_validation(train_set, valid_portion):
     return (train_set_x, train_set_y), (valid_set_x, valid_set_y)
 
 
+# 定义一个Data类
 class Data():
+    
+    # 初始化函数，传入数据、是否使用子图、使用的方法、是否使用稀疏矩阵、是否打乱数据
     def __init__(self, data, sub_graph=False, method='ggnn', sparse=False, shuffle=False):
+        
+        # 获取输入数据并进行mask处理
         inputs = data[0]
         inputs, mask, len_max = data_masks(inputs, [0])
-        self.inputs = np.asarray(inputs)
-        self.mask = np.asarray(mask)
-        self.len_max = len_max
-        self.targets = np.asarray(data[1])
-        self.length = len(inputs)
-        self.shuffle = shuffle
-        self.sub_graph = sub_graph
-        self.sparse = sparse
-        self.method = method
-
+        self.inputs = np.asarray(inputs)  # 转换为numpy数组
+        self.mask = np.asarray(mask)  # 转换为numpy数组
+        self.len_max = len_max  # 最大序列长度
+        self.targets = np.asarray(data[1])  # 获取目标数据并转换为numpy数组
+        self.length = len(inputs)  # 获取数据长度
+        self.shuffle = shuffle  # 是否打乱数据
+        self.sub_graph = sub_graph  # 是否使用子图
+        self.sparse = sparse  # 是否使用稀疏矩阵
+        self.method = method  # 使用的方法
+        
+    # 生成batch数据的函数，传入batch_size参数
     def generate_batch(self, batch_size):
+        
+        # 如果打乱数据
         if self.shuffle:
             shuffled_arg = np.arange(self.length)
-            np.random.shuffle(shuffled_arg)
-            self.inputs = self.inputs[shuffled_arg]
-            self.mask = self.mask[shuffled_arg]
-            self.targets = self.targets[shuffled_arg]
+            np.random.shuffle(shuffled_arg)  # 随机打乱索引
+            self.inputs = self.inputs[shuffled_arg]  # 根据打乱的索引更新输入数据
+            self.mask = self.mask[shuffled_arg]  # 根据打乱的索引更新mask数据
+            self.targets = self.targets[shuffled_arg]  # 根据打乱的索引更新目标数据
+        
+        # 计算batch数量
         n_batch = int(self.length / batch_size)
         if self.length % batch_size != 0:
             n_batch += 1
+            
+        # 将数据分成n_batch个batch，每个batch的数据量为batch_size
         slices = np.split(np.arange(n_batch * batch_size), n_batch)
+        
+        # 最后一个batch数据量不足batch_size，补齐
         slices[-1] = np.arange(self.length-batch_size, self.length)
+        
+        # 返回分好的batch数据
         return slices
+
 
     def get_slice(self, index):   #这里将输入数据转换成为图的结构矩阵
         if 1:
